14eea038415a4fe5b9014f8720409a138bf35769
Pablo <pablohuggem@gmail.com>
2022-11-14 13:59:02 -0300

    perf(quicksort): Remove un used imports

--- src/daa/sorting/quicksort.cpp
+++ src/daa/sorting/quicksort.cpp
@@ -1,11 +1,5 @@
-#include <pybind11/cast.h>
 #include <pybind11/chrono.h>
-#include <pybind11/complex.h>
-#include <pybind11/functional.h>
-#include <pybind11/iostream.h>
-#include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
-#include <pybind11/pytypes.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
 
