0af2f051ec03075cea09d456bfc2b4c005d692b7
Pablo <pablohuggem@gmail.com>
2022-11-23 15:46:08 -0300

    refactor(daa): change enum python binding

--- src/daa/sorting/quicksort.cpp
+++ src/daa/sorting/quicksort.cpp
@@ -18,7 +18,7 @@ using quicksort_string = libdaa::sorting::quicksort<std::string, char>;
 
 PYBIND11_MAKE_OPAQUE(std::vector<int>);
 PYBIND11_MAKE_OPAQUE(std::vector<float>);
-// PYBIND11_MAKE_OPAQUE(std::vector<std::string>);
+PYBIND11_MAKE_OPAQUE(std::vector<std::string>);
 
 PYBIND11_MODULE(daa, m) {
   m.doc() = "Design and Analysis of Algorithms";
@@ -26,49 +26,27 @@ PYBIND11_MODULE(daa, m) {
   auto m_sorting = m.def_submodule("sorting", "Sorting algorithms");
 
   // c++ enum class to python enum class
-  py::enum_<quicksort_vec_int::pivot>(m_sorting, "pivot_vec_int")
-    .value("FIRST", quicksort_vec_int::pivot::FIRST)
-    .value("LAST", quicksort_vec_int::pivot::LAST)
-    .value("MIDDLE", quicksort_vec_int::pivot::MIDDLE)
-    .value("MEDIAN_OF_THREE", quicksort_vec_int::pivot::MEDIAN_OF_THREE)
-    .export_values();
-
-  py::enum_<quicksort_vec_float::pivot>(m_sorting, "pivot_vec_float")
-    .value("FIRST", quicksort_vec_float::pivot::FIRST)
-    .value("LAST", quicksort_vec_float::pivot::LAST)
-    .value("MIDDLE", quicksort_vec_float::pivot::MIDDLE)
-    .value("MEDIAN_OF_THREE", quicksort_vec_float::pivot::MEDIAN_OF_THREE)
-    .export_values();
-
-  py::enum_<quicksort_vec_string::pivot>(m_sorting, "pivot_vec_string")
-    .value("FIRST", quicksort_vec_string::pivot::FIRST)
-    .value("LAST", quicksort_vec_string::pivot::LAST)
-    .value("MIDDLE", quicksort_vec_string::pivot::MIDDLE)
-    .value("MEDIAN_OF_THREE", quicksort_vec_string::pivot::MEDIAN_OF_THREE)
-    .export_values();
-
-  py::enum_<quicksort_string::pivot>(m_sorting, "pivot_string")
-    .value("FIRST", quicksort_string::pivot::FIRST)
-    .value("LAST", quicksort_string::pivot::LAST)
-    .value("MIDDLE", quicksort_string::pivot::MIDDLE)
-    .value("MEDIAN_OF_THREE", quicksort_string::pivot::MEDIAN_OF_THREE)
-    .export_values();
+  py::enum_<libdaa::sorting::pivot>(m_sorting, "pivot")
+    .value("FIRST", libdaa::sorting::pivot::FIRST)
+    .value("MIDDLE", libdaa::sorting::pivot::MIDDLE)
+    .value("LAST", libdaa::sorting::pivot::LAST)
+    .value("MEDIAN_OF_THREE", libdaa::sorting::pivot::MEDIAN_OF_THREE);
 
   // binding specific types to python
   py::class_<quicksort_vec_int>(m_sorting, "quicksort_int")
-    .def(py::init<quicksort_vec_int::pivot>())
+    .def(py::init<libdaa::sorting::pivot>())
     .def("sort", &quicksort_vec_int::sort);
 
   py::class_<quicksort_vec_float>(m_sorting, "quicksort_float")
-    .def(py::init<quicksort_vec_float::pivot>())
+    .def(py::init<libdaa::sorting::pivot>())
     .def("sort", &quicksort_vec_float::sort);
 
   py::class_<quicksort_vec_string>(m_sorting, "quicksort_vec_string")
-    .def(py::init<quicksort_vec_string::pivot>())
+    .def(py::init<libdaa::sorting::pivot>())
     .def("sort", &quicksort_vec_string::sort);
 
   py::class_<quicksort_string>(m_sorting, "quicksort_string")
-    .def(py::init<quicksort_string::pivot>())
+    .def(py::init<libdaa::sorting::pivot>())
     .def("sort", &quicksort_string::sort);
 
   // binding std::vector<int> to python
