29c37851ad6d11f34b04460ed12c162147c17517
Pablo <pablohuggem@gmail.com>
2023-04-26 18:52:39 -0300

    refactor(pydaa.cpp): remove unused 'greedy' module and add 'hashing_separate_chaining' and 'hashing' modules

--- src/pydaa/pydaa.cpp
+++ src/pydaa/pydaa.cpp
@@ -5,12 +5,14 @@ namespace py = pybind11;
 // modules initialization
 void sorting(py::module_ &m);
 void dynamic_programming(py::module_ &m);
-void greedy(py::module_ &m);
+void hashing_separate_chaining(py::module_ &m);
+void hashing(py::module_ &m);
 
 PYBIND11_MODULE(pydaa, m) {
     m.doc() = "Design and Analysis of Algorithms";
 
     sorting(m);
     dynamic_programming(m);
-    greedy(m);
+    hashing_separate_chaining(m);
+    hashing(m);
 }
