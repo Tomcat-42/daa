2a2eaab5e690d183ac1ac1a8875007609a730c2b
Pablo <pablohuggem@gmail.com>
2023-05-02 14:18:40 -0300

    refactor(open_addressing.hpp): initialize table vector with 0 instead of default constructor refactor(open_addressing.hpp): change entry_state::deleted to entry_state::occupied when inserting a key refactor(open_addressing.hpp): add print statement for number of comparisons in insert function refactor(main.cpp): remove unused code and hash function testing feat(main.cpp): add values vector and insert values into open_addressing_hash_table, print insertion results

--- include/daa/hashing/open_addressing.hpp
+++ include/daa/hashing/open_addressing.hpp
@@ -13,6 +13,7 @@ private:
     enum class entry_state : uint8_t {
         empty = 0,
         deleted = 1,
+        occupied = 2
     };
     std::vector<std::optional<Key>> table;
     std::vector<entry_state> entry_states;
@@ -22,7 +23,7 @@ private:
 
 public:
     open_addressing_hash_table(size_t size, HashFunc hash_func = HashFunc{})
-        : table(size), entry_states(size, entry_state::empty), hash_func(std::move(hash_func)), size(size) {}
+        : table(size, 0), entry_states(size, entry_state::empty), hash_func(std::move(hash_func)), size(size) {}
 
     auto insert(const Key& key) -> std::pair<bool, std::pair<size_t, size_t>> {
         size_t comparisons = 0;
@@ -33,12 +34,13 @@ public:
             size_t index = (hash_func(key) + i) % size;
             if (entry_states[index] == entry_state::empty || entry_states[index] == entry_state::deleted) {
                 table[index] = key;
-                entry_states[index] = entry_state::deleted;
+                entry_states[index] = entry_state::occupied;
                 auto end = std::chrono::steady_clock::now();
                 return std::make_pair(true, std::make_pair(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(), comparisons));
             }
         }
         auto end = std::chrono::steady_clock::now();
+        std::cout << "Comparisons: " << comparisons << std::endl;
         return std::make_pair(false, std::make_pair(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(), comparisons));
     }
 
--- src/main.cpp
+++ src/main.cpp
@@ -13,101 +13,70 @@
 #include <chrono>
 
 auto main() -> int {
-    std::mt19937 gen(std::random_device{}());
-    std::uniform_int_distribution<> dist(1, 1000);
+    std::vector<int> values = {
+        2,
+        63,
+        38,
+        22,
+        25,
+        50,
+        20,
+        8,
+        88,
+        97,
+        94,
+        19,
+        56,
+        39,
+        41,
+        83,
+        95,
+        74,
+        4,
+        89,
+        91,
+        76,
+        71,
+        3,
+        75,
+        53,
+        7,
+        96,
+        68,
+        10,
+        70,
+        48,
+        82,
+        29,
+        33,
+        55,
+        24,
+        100,
+        34,
+        77,
+        72,
+        62,
+        1,
+        14,
+        59,
+        87,
+        32,
+        79,
+        90,
+        43
+    };
 
-    std::vector<int> keys(1000);
-    std::generate(keys.begin(), keys.end(), [&]() {
-        return dist(gen);
+
+
+    daa::hashing::open_addressing_hash_table<int, std::function<int(int)>> open_addressing_table(50, [](int key) {
+        return key % 10;
     });
 
-    auto likely_collision_hash_func = [](int key) {
-        return key % 10;
-    };
-
-    // https://en.wikipedia.org/wiki/MurmurHash
-    auto unlikely_collision_hash_func = [](int key) -> size_t {
-        // MurmurHash3_x86_32 as a lambda function (adapted from the public domain MurmurHash3 implementation)
-        const uint32_t c1 = 0xcc9e2d51;
-        const uint32_t c2 = 0x1b873593;
-        const uint32_t seed = 0;
-        const int r1 = 15;
-        const int r2 = 13;
-        const uint32_t m = 5;
-        const uint32_t n = 0xe6546b64;
-
-        uint32_t h1 = seed;
-        auto k1 = static_cast<uint32_t>(key);
-        k1 *= c1;
-        k1 = (k1 << r1) | (k1 >> (32 - r1));
-        k1 *= c2;
-
-        h1 ^= k1;
-        h1 = (h1 << r2) | (h1 >> (32 - r2));
-        h1 = h1 * m + n;
-
-        h1 ^= 4;
-        h1 ^= h1 >> 16;
-        h1 *= 0x85ebca6b;
-        h1 ^= h1 >> 13;
-        h1 *= 0xc2b2ae35;
-        h1 ^= h1 >> 16;
-
-        return h1;
-    };
-
-    // hash functions to string
-    std::vector<std::pair<std::function<size_t(int)>, std::string>> hash_functions = {
-        {likely_collision_hash_func, "likely collision"},
-        {unlikely_collision_hash_func, "unlikely collision"}
-    };
-
-    for (const auto& [hash_func, hash_func_name] : hash_functions) {
-        std::cout << "Testing with " << hash_func_name << " hash function" << std::endl;
-
-        daa::hashing::open_addressing_hash_table<int, std::function<size_t(int)>> oa_hash_table(keys.size(), hash_func);
-        daa::hashing::separate_chaining_hash_table<int, std::function<size_t(int)>> sc_hash_table(keys.size(), hash_func);
-
-        size_t oa_insertion_time = 0, sc_insertion_time = 0;
-        size_t oa_key_comparisons_insert = 0, sc_key_comparisons_insert = 0;
-
-        for (const auto& key : keys) {
-            auto oa_insert_result = oa_hash_table.insert(key);
-            oa_insertion_time += oa_insert_result.second.first;
-            oa_key_comparisons_insert += oa_insert_result.second.second;
-
-            auto sc_insert_result = sc_hash_table.insert(key);
-            sc_insertion_time += sc_insert_result.second.first;
-            sc_key_comparisons_insert += sc_insert_result.second.second;
-        }
-
-        std::shuffle(keys.begin(), keys.end(), gen);
-
-        size_t oa_search_time = 0, sc_search_time = 0;
-        size_t oa_key_comparisons_search = 0, sc_key_comparisons_search = 0;
-
-        for (const auto& key : keys) {
-            auto oa_search_result = oa_hash_table.search(key);
-            oa_search_time += oa_search_result.second.first;
-            oa_key_comparisons_search += oa_search_result.second.second;
-
-            auto sc_search_result = sc_hash_table.search(key);
-            sc_search_time += sc_search_result.second.first;
-            sc_key_comparisons_search += sc_search_result.second.second;
-        }
-
-        std::cout << "Open addressing: \n"
-                  << "  Total insertion time: " << oa_insertion_time << " ns\n"
-                  << "  Total key comparisons (insert): " << oa_key_comparisons_insert << "\n"
-                  << "  Total search time: " << oa_search_time << " ns\n"
-                  << "  Total key comparisons (search): " << oa_key_comparisons_search << "\n";
-
-        std::cout << "Separate chaining: \n"
-                  << "  Total insertion time: " << sc_insertion_time << " ns\n"
-                  << "  Total key comparisons (insert): " << sc_key_comparisons_insert << "\n"
-                  << "  Total search time: " << sc_search_time << " ns\n"
-                  << "  Total key comparisons (search): " << sc_key_comparisons_search << "\n";
+    for (auto& value : values) {
+        auto res = open_addressing_table.insert(value);
+        // print (bool, (time, comps))
+        std::cout << res.first<< std::endl;
+        std::cout << res.second.first << std::endl;
+        std::cout << res.second.second << std::endl;
     }
-
-    return 0;
 }
